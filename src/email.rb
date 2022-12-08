e8dae537c47669ac43cd56fe7541ad852a99b189
chabanyukm <chabanyuk.maks@gmail.com>
2022-12-08 17:55:37 +0200

    added functionality to check if new email address adds to all_emails

--- src/email.rb
+++ src/email.rb
@@ -1,4 +1,6 @@
 require_relative 'category'
+require_relative 'letter'
+require 'date'
 
 class EmailError < StandardError
    def initialize(msg = "Error working with emails")
@@ -15,14 +17,18 @@ class Email
    def initialize(email_address)
       if email_address.is_a? String
          if email_address.include? "@"
-            @email = email_address
-            @categories = {
-               :spam => Category.new('spam'),
-               :inbox => Category.new('inbox'),
-               :sent => Category.new('sent')
-            }
-            @@all_emails[@email] = self
-            @spammers = []
+            if @@all_emails.include? email_address
+               raise EmailError("There is already email with address '#{email_address}'")
+            else
+               @email = email_address
+               @categories = {
+                  :spam => Category.new('spam'),
+                  :inbox => Category.new('inbox'),
+                  :sent => Category.new('sent')
+               }
+               @@all_emails[@email] = self
+               @spammers = []
+            end
          else
             raise EmailError("Please provide a valid email address")
          end
@@ -31,8 +37,12 @@ class Email
       end
    end
 
-   def add_category(name)
+   def create_category(name)
       @categories[name.to_sym] = Category.new(name)
+   end
+
+   def get_categories
+      @categories.keys
    end
 
    def move_to_users_category(letter, category_name)
@@ -61,7 +71,27 @@ class Email
       end
    end
 
+   def send_letter(to, title, body, tags, date=DateTime.now)
+      if @@all_emails.keys.include? to
+         letter = Letter(@email, to, title, body, tags, date)
+         @@all_emails[to].receive_letter(letter)
+         @categories[:sent].push(letter)
+      end
+   end
+
    def self.get_all_categories
       return @@all_emails.keys
+   end
+
+   def self.find_email(email_address)
+      if @@all_emails.include? email_address
+         @@all_emails[email_address]
+      else
+         raise EmailError("There is no email with address '#{email_address}'")
+      end
+   end
+
+   def self.include?(email_address)
+      @@all_emails.include? email_address
    end
 end
--- test/test_zalik.rb
+++ test/test_zalik.rb
@@ -8,10 +8,16 @@ class TestZalik < Minitest::Test
     refute_nil ::Zalik::VERSION
   end
 
+  def before_setup
+    super
+    unless Email.include? 'ccc@gmail.com'
+      a = Email.new('aaa@gmail.com')
+      b = Email.new('bbb@gmail.com')
+      c = Email.new('ccc@gmail.com')
+    end
+  end
+
   def test_it_does_something_useful
-    a = Email.new('aaa@gmail.com')
-    b = Email.new('bbb@gmail.com')
-    c = Email.new('ccc@gmail.com')
     self.assert_equal(%w[aaa@gmail.com bbb@gmail.com ccc@gmail.com], Email.get_all_categories)
   end
 
@@ -28,5 +34,18 @@ class TestZalik < Minitest::Test
     rescue
       assert true
     end
+    begin
+      a = Email.find_email('aaa@gmail.com')
+      a.create_category(9898)
+      assert false, "there must be unavailable to create a category with invalid name"
+    rescue
+      assert true
+    end
+  end
+
+  def test_category
+    a = Email.find_email('aaa@gmail.com')
+    a.create_category('favorite')
+    assert_equal([:spam, :inbox, :sent, :favorite], a.get_categories, 'Expected a new category to be added')
   end
 end
