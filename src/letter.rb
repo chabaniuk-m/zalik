970d7d76227f2c35be5788b1bdc245cb79ddb286
chabanyukm <chabanyuk.maks@gmail.com>
2022-12-08 18:19:00 +0200

    implemented letters equality check

--- src/letter.rb
+++ src/letter.rb
@@ -3,7 +3,6 @@ require 'date'
 class Letter
 
    attr_reader :from, :to, :title, :body, :date
-   attr_accessor :category
 
    def initialize(from, to, title, body, tags, date=DateTime.now)
       @from = from
@@ -12,6 +11,15 @@ class Letter
       @body = body
       @tags = tags
       @date = date
-      @category = nil
+   end
+
+   def ==(other)
+      if other == nil
+         false
+      elsif other.is_a? Letter
+         other.date == self.date and other.from == self.from and other.to == self.to and other.title == self.title and other.body == self.body
+      else
+         false
+      end
    end
 end
